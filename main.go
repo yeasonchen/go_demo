eb69e3cd036a343178b04206f6008402a73f0862
yeasonchen <yeasonchen@tencent.com>
2022-08-12 15:00:21 +0800

    添加内容

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, v := range nums {
+		if i2, ok := m[target-v]; ok {
+			return []int{i, i2}
+		}
+		m[v] = i
+	}
+	return []int{}
+}
+
+func main() {
+
+}
